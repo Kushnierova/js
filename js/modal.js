93bacb94c4a91f97aa052f56035a48610200638c
Kushnierova <Katerynakushnierova@gmail.com>
2023-05-05 11:00:02 +0200

    10:59

--- js/modal.js
+++ js/modal.js
@@ -1,49 +1,57 @@
 let books = [
   {
-    id:"1",
-    title: 'The Lord Of Ring',
-    author: 'John Ronald Reuel Tolkien',
-    img:'https://ecsmedia.pl/c/the-lord-of-the-rings-w-iext127966933.jpg',
-    plot: 'Frodo Baggins, a hobbit, must destroy the Ring and prevent Sauron, the dark lord, from getting it and regaining his former power. We are given a basic good versus evil plot and are thrown into the Middle-World, a place that is home to hobbits, elves, dwarves, humans, wizards, and goblins.',
+    id: "1",
+    title: "The Lord Of Ring",
+    author: "John Ronald Reuel Tolkien",
+    img: "https://ecsmedia.pl/c/the-lord-of-the-rings-w-iext127966933.jpg",
+    plot: "Frodo Baggins, a hobbit, must destroy the Ring and prevent Sauron, the dark lord, from getting it and regaining his former power. We are given a basic good versus evil plot and are thrown into the Middle-World, a place that is home to hobbits, elves, dwarves, humans, wizards, and goblins.",
   },
   {
-    id:"2",
-    title: 'The Witcher',
-    author: 'Andrzej Sapkowski',
-    img:'https://emp-scs-uat.img-osdw.pl/img-p/1/kipwn/c0aac775/std/e6-172/820228109o.jpg',
-    plot: 'Geralt of Rivia is a Witcher. Trained at a young age at the stronghold of the Wolf School Witchers, he survived numerous brutal physical challenges to gain near-superhuman physical and mental abilities. Now he travels the Continent in the aftermath of the first major war between the Nilfgaard Empire and the Northern Kingdoms, killing monsters that hunt the lands.',
+    id: "2",
+    title: "The Witcher",
+    author: "Andrzej Sapkowski",
+    img: "https://emp-scs-uat.img-osdw.pl/img-p/1/kipwn/c0aac775/std/e6-172/820228109o.jpg",
+    plot: "Geralt of Rivia is a Witcher. Trained at a young age at the stronghold of the Wolf School Witchers, he survived numerous brutal physical challenges to gain near-superhuman physical and mental abilities. Now he travels the Continent in the aftermath of the first major war between the Nilfgaard Empire and the Northern Kingdoms, killing monsters that hunt the lands.",
   },
   {
-    id:"3",
-    title: 'Forest Song',
-    author: 'Larisa Petrovna Kosach-kvitka',
-    img: 'https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1538362623l/42107352._SX318_.jpg',
-    plot:'A forest nymph Mavka falls in love with a mortal youth Lukash causing a dramatic clash between the world of carefree beauty of the forest and the world of the humans filled with jealousy, passion, rivalry and evil.',
+    id: "3",
+    title: "Forest Song",
+    author: "Larisa Petrovna Kosach-kvitka",
+    img: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1538362623l/42107352._SX318_.jpg",
+    plot: "A forest nymph Mavka falls in love with a mortal youth Lukash causing a dramatic clash between the world of carefree beauty of the forest and the world of the humans filled with jealousy, passion, rivalry and evil.",
   },
-]
+];
 
-const root = document.getElementById('root');
+const root = document.getElementById("root");
 // const root = document.querySelector('#root');
 // console.log(root)
 
-const div1 = document.createElement('div')
-const div2 = document.createElement('div');
+const div1 = document.createElement("div");
+const div2 = document.createElement("div");
 
-div1.classList.add('left')
-div2.classList.add('right');
+div1.classList.add("left");
+div2.classList.add("right");
 
 root.append(div1, div2);
-const title = document.createElement('h1')
-title.textContent = 'Libruary'
+const title = document.createElement("h1");
+title.textContent = "Libruary";
 
-const list = document.createElement('ul')
-const addBtn = document.createElement('button')
-addBtn.textContent = "Add"
+const list = document.createElement("ul");
+list.classList.add('list')
 
-div1.append(title, list, addBtn)
+const addBtn = document.createElement("button");
+addBtn.textContent = "Add";
+addBtn.classList.add('add')
 
-function renderList () {
-  const markup = books.map(({id, title}) =>'<li id=${id}><p>${title}</p><button>Delite</button><button>Edit</button></li>').join('')
-  list.insertAdjacentHTML("beforeend", markup)
+div1.append(title, list, addBtn);
+
+function renderList() {
+  const markup = books
+    .map(
+      ({ id, title }) =>
+        `<li id=${id} class='item'><p class='item-title'>${title}</p><button class='delete'>Delete</button><button class='edit'>Edit</button></li>`
+    )
+    .join("");
+  list.insertAdjacentHTML("beforeend", markup);
 }
-renderList()
+renderList();

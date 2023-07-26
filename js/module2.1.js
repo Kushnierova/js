0ba6510738679dd90fffa2d17f219ccd6678b0bb
Kushnierova <Katerynakushnierova@gmail.com>
2023-07-26 17:08:37 +0200

    17:08

---
+++ js/module2.1.js
@@ -0,0 +1,124 @@
+const friends = [`Monica Geller`, `Lily Aldrin`, `Barney Stinson`, `Ted Mosby`];
+
+// console.log(friends);
+// console.table виведення масива табличкою;
+// console.table(friends);
+
+// .length - довжина з 0(перший елемент це 0);
+// console.log(friends.length);
+
+// Знайти останній індекс;
+// const lastIndex = friends.length - 1;
+// console.log(lastIndex);
+
+// Звернення до елементу масива;
+// console.log([`Monica`, `Rachel`, `Joey`, `Phoebe`, `Chandler`, `Ross`][0]);
+console.log(friends[0]);
+
+//Змінити ячейку в масиві;
+friends[3] = `Chandler Bing`;
+
+// Метод .split
+// const name = "Monica";
+// console.log(name.split("")); // ['M', 'o', 'n', 'i', 'c', 'a']
+
+const message = "Monica, Rachel, Joey, Phoebe, Chandler, Ros";
+console.log(message.split(", ")); // ['Monica', 'Rachel', 'Joey', 'Phoebe', 'Chandler', 'Ros']
+
+// Метод indexOf()
+// indexOf(value) повертає перший індекс, в якому елемент зі значенням value
+// був знайдений в масиві, або число -1, якщо такий елемент відсутній.
+//  Використовуйте indexOf тоді, коли необхідно отримати сам індекс елемента.
+
+console.log(friends.indexOf("Monica Geller")); // 0
+console.log(friends.indexOf("Robin Scherbatsky")); // -1
+
+// Метод includes()
+// includes(value) перевіряє, чи містить масив елемент зі значенням value і
+// повертає true або false відповідно. Застосування цього методу корисне в ситуаціях,
+// коли необхідно перевірити, чи є елемент в масиві і не важлива його позиція (індекс).
+
+console.log(friends.includes("Monica Geller")); // true
+console.log(friends.includes("Marshall Eriksen")); // false
+
+console.table(friends);
+
+// Метод slice()
+// slice(begin, end) повертає новий масив,
+//  що містить копію частини вихідного масиву, не змінюючи його.
+console.log(friends.slice(1, 3)); // ['Lily Aldrin', 'Barney Stinson']
+
+// Метод splice()
+
+// Щоб видалити елементи в масиві, передаються два аргументи.
+// splice(position, num)
+
+const scores = [1, 2, 3, 4, 5];
+
+// Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
+const deletedScores = scores.splice(0, 3);
+
+// Тепер масив scores містить два елементи
+console.log(scores); // [4, 5]
+
+// А масив deletedScores містить три видалені елементи
+console.log(deletedScores); // [1, 2, 3]
+
+// Для того, щоб додати один або декілька елементів в масив,
+// необхідно передати три або більше аргументи, за такої умови,
+//  другий аргумент повинен дорівнювати нулю.
+
+// splice(position, 0, new_element_1, new_element_2, ...)
+friends.splice(5, 0, "Ross Geller");
+console.log(friends); // ['Monica Geller', 'Lily Aldrin', 'Barney Stinson', 'Chandler Bing', 'Ross Geller']
+
+// Заміна - це операція додавання, в якій видаляються елементи в місці додавання нових.
+// splice(position, num, new_element_1, new_element_2, ...)
+
+friends.splice(2, 1, "Phoebe Buffay");
+console.log(friends); // ['Monica Geller', 'Lily Aldrin', 'Phoebe Buffay', 'Chandler Bing', 'Ross Geller']
+
+// Заміняємо один елемент (з індексом 2) на декілька.
+
+friends.splice(1, 1, `Rachel Green`, `Joey Tribbiani`);
+console.log(friends); // ['Monica Geller', 'Rachel Green', 'Joey Tribbiani', 'Phoebe Buffay', 'Chandler Bing', 'Ross Geller']
+
+// ЛЕКЦІЯ
+
+const a = [1, 2, 3];
+const b = a;
+
+console.log(`a:`, a); //[1, 2, 3];
+console.log(`b:`, b); //[1, 2, 3];
+
+a[0] = 500;
+
+console.log(`a:`, a); //[500, 2, 3];
+console.log(`b:`, b); //[500, 2, 3];
+
+console.log(a === b); //true
+// Тому що в змінних лежить одне й те саме посилання на масив.
+
+console.log([1, 2, 3] === [1, 2, 3]); //false
+//Тому що посилання на різні ячейки в пам'яті
+
+// Перебор по ітерації масива
+
+//Так робити не треба, неможливо перебрати масив з 1000 елементів
+// console.log(friends[0]);
+// console.log(friends[1]);
+// console.log(friends[2]);
+// console.log(friends[3]);
+// console.log(friends[4]);
+// console.log(friends[5]);
+
+// for (let i = 0; i < friends.length; i += 1) {
+//   friends[i] += `*`;
+//   console.log(friends[i]);
+// }
+
+//Краще робити так(якщо не потрібен індекс,або не потрібно змінювати елемент)
+
+// for (const friend of friends) {
+//   console.log(friend);
+// }

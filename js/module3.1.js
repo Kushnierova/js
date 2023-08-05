9c0ea2e264a17d82761471c98b233c689af8c639
Kushnierova <Katerynakushnierova@gmail.com>
2023-08-05 20:10:28 +0200

    20:10

---
+++ js/module3.1.js
@@ -0,0 +1,149 @@
+// const booklist = {
+//   name: `Мої книжки`,
+//   readBooks: [`Відьмак`, "Гоббіт", "Володар перснів", "Сільмаріліон", "Дюна"],
+// };
+// // Створення нової
+// booklist.unReadBooks = [`Проклята`, "Із крові і попелу"];
+// // Перевизначення вже існуючого
+// booklist.name = `Книжки для мене`;
+
+// console.log(booklist);
+// console.log(booklist.name);
+// console.log(booklist.readBooks);
+
+// const propertyName = `name`;
+// // Два записи знизу є ідентичними
+// console.log(booklist[`name`]);
+// console.log(booklist[propertyName]);
+
+// // ===========================
+// const username = `Mango`;
+// const email = `mango@gmail.com`;
+
+// const signupData = {
+//   //   username: username,
+//   //   email: email,
+//   // внизу ідентичний запис(якщо ім'я ключа таке саме як ім'я змінної)
+
+//   username,
+//   email,
+// };
+
+// console.log(signupData);
+// ===================================================================
+// // Властивості які обчислюються
+// // <input name=`color` value=`tomato`
+// const inputName = `color`;
+// const inputValue = `tomato`;
+
+// const colorPickerData = {
+//   [inputName]: inputValue,
+// };
+// console.log(colorPickerData); //{color: 'tomato'}
+
+// ===================================================================
+// // Тип посилання {}==={}
+// console.log([] === []); //false
+// const a = { x: 1, y: 2 };
+// const b = a;
+// console.log({ x: 1, y: 2 } === { x: 1, y: 2 }); //false
+// console.log(b === a); //true
+// ===================================================================
+// Масиви і функції це теж ОБ'ЄКТИ
+// Arrays
+// const a = [1, 2, 3];
+
+// a.hello = `:)`;
+
+// console.log(a);
+// // Functions
+
+// const fn = function () {
+//   console.log("hello");
+// };
+// fn.hello = `:)`;
+// console.dir(fn.hello);
+
+// ==========================================================================
+// // Методи об'єкту і this при зверненні до властивостей в методах
+
+// const booklist = {
+//   name: `Книжки для мене`,
+//   readBooks: [`Відьмак`, "Гоббіт", "Володар перснів", "Сільмаріліон", "Дюна"],
+//   unReadBooks: [`Проклята`, "Із крові і попелу"],
+//   //   //   getName: function () {
+//   //   //     console.log("Так це getName:)");
+//   //   //   },
+//   //   // Це стара форма запису,далі нова форма МЕТОДУ ОБ'ЄКТА
+//   //   getName() {
+//   //     console.log("Так це getName:)");
+//   //   },
+//   changeName(newName) {
+//     // console.log(`this всередині changeName:`, this);
+//     this.name = newName;
+//   },
+//   addReadBook(newBook) {
+//     this.readBooks.push(newBook);
+//   },
+//   addUnReadBooks(newBook) {
+//     this.unReadBooks.push(newBook);
+//     this.countUnReadBooks = this.unReadBooks.length;
+//   },
+//   getCountReadBooks() {
+//     return this.readBooks.length;
+//   },
+//   getCountUnReadBooks() {
+//     return this.unReadBooks.length;
+//   },
+// };
+
+// console.log(booklist.getCountReadBooks()); //5
+// // console.log(booklist);
+// // booklist.getName();
+// booklist.changeName(`My books`);
+// // console.log(booklist);
+
+// booklist.addReadBook(`Бути чи мати`);
+// booklist.addUnReadBooks(`Два капітани`);
+// console.log(booklist);
+
+// console.log(booklist.getCountReadBooks()); //6
+
+// ========================================================================
+// Перебор об'єкта з допомогою for ... in і Object.keys|value|entries
+const feedback = {
+  good: 5,
+  neutral: 10,
+  bad: 3,
+};
+let totalFeedback = 0;
+// Отримуємо keys
+// const keys = Object.keys(feedback);
+
+// console.log(keys);
+
+// for (const key of keys) {
+//   console.log(key);
+//   //   Пошук value
+//   console.log(feedback[key]);
+
+//   totalFeedback += feedback[key];
+// }
+// console.log(`totalFeedback:`,totalFeedback);
+// ========================
+// Отримуємо value
+
+// const values = Object.values(feedback)
+
+// for(const value of values){
+//     totalFeedback += value;
+// }
+// console.log(`totalFeedback:`,totalFeedback);
+// // ==============================
+// // Отримуємо масив [ключ б значення]
+// const entries = Object.entries(feedback)
+
+// for(const entrie of entries){
+//     console.log(entrie)
+// }
+
